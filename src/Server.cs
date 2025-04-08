6eb58fa8603f90037f144457c5ab1ad2aa24b631
Saul Lara <hl.saul.01@gmail.com>
2025-04-07 23:26:26 -0600

    📦 NEW: Send a response string depending on the path.

--- src/Server.cs
+++ src/Server.cs
@@ -10,5 +10,29 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket socket = server.AcceptSocket(); // wait for client
 
-//Sends a 200 response string to the connected client.
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+// Read request
+byte[] httpRequest = new byte[1024];
+socket.Receive(httpRequest);
+
+string requestData = Encoding.UTF8.GetString(httpRequest);
+
+string[] httpRequestParts = requestData.Split("\r\n");
+string requestLine = httpRequestParts[0];
+string[] requestLineParts = requestLine.Split(" ");
+var (httpMethod, requestTarget, httpVersion) = (requestLineParts[0], requestLineParts[1], requestLineParts[2]);
+
+Dictionary<string, string> responses = new Dictionary<string, string>
+{
+  {"/", "HTTP/1.1 200 OK\r\n\r\n" }
+};
+
+if (responses.ContainsKey(requestTarget))
+{
+    //Sends a response string to the connected client.
+    socket.Send(Encoding.UTF8.GetBytes(responses[requestTarget]));
+}
+else
+{
+    //Sends a 400 response string to the connected client.
+    socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+}
