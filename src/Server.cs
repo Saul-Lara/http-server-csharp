fcaa42810623d12622324de2b4df29061ad4e5ef
Saul Lara <hl.saul.01@gmail.com>
2025-04-08 21:51:51 -0600

    📦 NEW: Add /echo endpoint with dynamic message and content-length

--- src/Server.cs
+++ src/Server.cs
@@ -20,19 +20,33 @@ string[] httpRequestParts = requestData.Split("\r\n");
 string requestLine = httpRequestParts[0];
 string[] requestLineParts = requestLine.Split(" ");
 var (httpMethod, requestTarget, httpVersion) = (requestLineParts[0], requestLineParts[1], requestLineParts[2]);
+string urlPath = requestTarget.IndexOfAny("/".ToCharArray(), 1) == -1 ? requestTarget : requestTarget.Substring(0, requestTarget.IndexOfAny("/".ToCharArray(), 1));
 
 Dictionary<string, string> responses = new Dictionary<string, string>
 {
-  {"/", "HTTP/1.1 200 OK\r\n\r\n" }
+  {"/", "HTTP/1.1 200 OK\r\n\r\n" },
+  {"/echo", "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %length%\r\n\r\n%message%" }
 };
 
-if (responses.ContainsKey(requestTarget))
+if (responses.ContainsKey(urlPath))
 {
-    //Sends a response string to the connected client.
-    socket.Send(Encoding.UTF8.GetBytes(responses[requestTarget]));
+  //Sends a response string to the connected client.
+  if (urlPath == "/")
+  {
+    socket.Send(Encoding.UTF8.GetBytes(responses[urlPath]));
+  }
+  else if (urlPath == "/echo")
+  {
+    string data = requestTarget.Replace("/echo/", "");
+    string responseTemplate = responses[urlPath];
+    responseTemplate = responseTemplate.Replace("%length%", data.Length.ToString()); // Replacing content-length
+    responseTemplate = responseTemplate.Replace("%message%", data);                  // Replacing response body
+    socket.Send(Encoding.UTF8.GetBytes(responseTemplate));
+  }
+
 }
 else
 {
-    //Sends a 400 response string to the connected client.
-    socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+  //Sends a 400 response string to the connected client.
+  socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
 }
