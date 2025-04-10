4b4b166751f533b04d7ddde894b225a9350ae5ee
Saul Lara <hl.saul.01@gmail.com>
2025-04-10 01:39:55 -0600

    📦 NEW: Add /files endpoint with filename reading and --directory flag support

--- src/Server.cs
+++ src/Server.cs
@@ -1,15 +1,28 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Runtime.InteropServices;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
+
+string[] arguments = Environment.GetCommandLineArgs();
+
+// If no additional arguments are passed, use a default temporary directory path based on the operating system.
+string tmpDirectoryPath = arguments.Length == 1 ? RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"C:\tmp\" : "/tmp/" : arguments[2];
+
+if (!Directory.Exists(tmpDirectoryPath))
+{
+  Directory.CreateDirectory(tmpDirectoryPath);
+  Console.WriteLine("The temporary directory was created successfully");
+}
 
 Dictionary<string, string> responses = new Dictionary<string, string>
 {
   {"/", "HTTP/1.1 200 OK\r\n\r\n" },
   {"/echo", "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %length%\r\n\r\n%message%" },
-  {"/user-agent" , "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %length%\r\n\r\n%message%"}
+  {"/user-agent" , "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %length%\r\n\r\n%message%"},
+  {"/files", "HTTP/1.1 200 OK\r\nContent-Type: %mime_type%\r\nContent-Length: %length%\r\n\r\n%message%"}
 };
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
@@ -62,11 +75,32 @@ void handleRequest(Socket socket, Dictionary<string, string> responses)
       responseTemplate = responseTemplate.Replace("%message%", userAgentValue);                  // Replacing response body
       socket.Send(Encoding.UTF8.GetBytes(responseTemplate));
     }
+    else if (urlPath == "/files")
+    {
+      string fileNameRequested = requestTarget.Replace("/files/", "");
+      string filePath = String.Concat(tmpDirectoryPath, fileNameRequested);
 
+      if (File.Exists(filePath) && fileNameRequested.Length > 0)
+      {
+        string fileExtension = Path.GetExtension(filePath);
+        string mimeType = MimeTypes.GetMimeTypeOrDefault(fileExtension, "application/octet-stream");
+        string fileContent = File.ReadAllText(filePath);  // Open the file to read from.
+
+        string responseTemplate = responses[urlPath];
+        responseTemplate = responseTemplate.Replace("%mime_type%", mimeType);                     // Replacing content-type
+        responseTemplate = responseTemplate.Replace("%length%", fileContent.Length.ToString());   // Replacing content-length
+        responseTemplate = responseTemplate.Replace("%message%", fileContent);                    // Replacing response body
+        socket.Send(Encoding.UTF8.GetBytes(responseTemplate));
+      }
+      else
+      {
+        socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+      }
+    }
   }
   else
   {
-    //Sends a 400 response string to the connected client.
+    //Sends a 404 response string to the connected client.
     socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
   }
 
